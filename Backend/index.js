81d8ea3534466bf37d6d7f2e161e3d04534fb5ec
evagurubi <evagurubi@gmail.com>
2021-03-29 13:50:30 +0200

    Basic data structure and backend added.

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+//const articles = require("./articles.json");
+
+//let articlesDirectory = articles;
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
